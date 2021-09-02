c3adb33668dfa24b033839bd82265ffd8dc2cd48
Ümit Dengiz <umit.dengiz@enqura.com>
2021-09-02 18:13:34 +0300

    Create Account - Get User Info

---
+++ Udemy.BankApp.Web/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Udemy.BankApp.Web.Data.Context;
+
+namespace Udemy.BankApp.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly BankContext _bankContext;
+
+        public AccountController(BankContext bankContext)
+        {
+            _bankContext = bankContext;
+        }
+
+        public IActionResult Create(int id)
+        {
+            var userInfo = _bankContext.ApplicationUsers.SingleOrDefault(x => x.Id == id);
+
+            return View(userInfo);
+        }
+    }
+}
