2ff7e8039971ee3cccfc6d4a1fbd4a826421d110
Ümit Dengiz <umit.dengiz@enqura.com>
2021-09-02 17:37:23 +0300

    Get Account Count

---
+++ Udemy.BankApp.Web/TagHelpers/GetAccountCount.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Linq;
+using Udemy.BankApp.Web.Data.Context;
+
+namespace Udemy.BankApp.Web.TagHelpers
+{
+    [HtmlTargetElement("getAccountCount")]
+    public class GetAccountCount : TagHelper
+    {
+        public int ApplicationUserId { get; set; }
+
+        private readonly BankContext _bankContext;
+        public GetAccountCount(BankContext bankContext)
+        {
+            _bankContext = bankContext;
+        }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var accountCount = _bankContext.Accounts.Count(x => x.ApplicationUserId == ApplicationUserId);
+            var html = $"<span class='badge bg-danger'>{accountCount}</span>";
+
+            output.Content.SetHtmlContent(html);
+        }
+    }
+}
